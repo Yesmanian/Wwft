cd3fe3abb44d53e66be93698f48bd9226db23a91
bitcamp <bitcamp@DESKTOP-6REN0OO>
2021-01-14 20:44:21 +0900

    Sin : Tree.java , TreeDao.java, TreeService.java, TreeServiceImpl.java, TreeDaoImpl.java 생성 및 작성 중

--- Wwft/src/main/java/com/wwft/service/tree/impl/TreeServiceImpl.java
+++ Wwft/src/main/java/com/wwft/service/tree/impl/TreeServiceImpl.java
@@ -1,11 +1,41 @@
 package com.wwft.service.tree.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.wwft.service.domain.Tree;
+import com.wwft.service.tree.TreeDao;
 import com.wwft.service.tree.TreeService;
 
+@Service("treeServiceImpl")
 public class TreeServiceImpl implements TreeService {
 
-	public TreeServiceImpl() {
-		// TODO Auto-generated constructor stub
+	@Autowired
+	@Qualifier("treeDaoImpl")
+	private TreeDao treeDao;
+	public void setTreeDao(TreeDao treeDao) {
+		this.treeDao = treeDao;
 	}
+	
+	
+	public TreeServiceImpl() {
+		
+		System.out.println("::"+getClass()+"default Constructor Call..");
+	}
+
+	@Override
+	public void addBucketList(Tree tree) throws Exception {
+		treeDao.addBucketList(tree);
+		
+	}
+
+	@Override
+	public void addFamilyMotto(Tree tree) throws Exception {
+		treeDao.addFamilyMotto(tree);
+		
+	}
+	
+	
 
 }
