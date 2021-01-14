682cecb480cc23f348bdb671cef796e38fae5f3b
koo <koo@DESKTOP-L9F67HH>
2021-01-14 20:47:24 +0900

    gu  :  User,Profile  -Dao,DaoImpl,Service,ServiceImpl .java        User,Profile  -Mapper.xml
                     생성

--- Wwft/src/main/java/com/wwft/service/user/impl/UserServiceImpl.java
+++ Wwft/src/main/java/com/wwft/service/user/impl/UserServiceImpl.java
@@ -1,11 +1,78 @@
 package com.wwft.service.user.impl;
 
+import java.util.Map;
+
+import com.wwft.common.web.Search;
+import com.wwft.service.domain.User;
 import com.wwft.service.user.UserService;
 
 public class UserServiceImpl implements UserService {
 
-	public UserServiceImpl() {
-		// TODO Auto-generated constructor stub
+	@Override
+	public void addUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
+
+	@Override
+	public User getUser(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getUserList(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findId(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findPassword(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void userWithdrawal(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void userActivation(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkDuplication(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User Certified(User userId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User Authentication(User userId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
