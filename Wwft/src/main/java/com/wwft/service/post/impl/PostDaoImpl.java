4133a9f44e763f682d65406b96c52bbf8f28a017
dkgur <dkgur@DESKTOP-09188DF>
2021-01-15 10:44:48 +0900

    cho: PostDaoImpl

--- Wwft/src/main/java/com/wwft/service/post/impl/PostDaoImpl.java
+++ Wwft/src/main/java/com/wwft/service/post/impl/PostDaoImpl.java
@@ -12,7 +12,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
-
+//
 @Repository("postDaoImpl")
 public class PostDaoImpl implements PostDao {
 	
